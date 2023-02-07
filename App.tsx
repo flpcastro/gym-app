03452980b44c0faa703e1f48e6f30de5484dc0e3
Felipe <flpc.dev@gmail.com>
2023-02-06 22:36:43 -0300

    feat: added assets folder and editing status bar

--- App.tsx
+++ App.tsx
@@ -1,11 +1,17 @@
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
-import { Text, View } from 'react-native';
+import { StatusBar, Text, View } from 'react-native';
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
 
   return (
-    <View>
+    <View style={{ flex: 1, alignItems: 'center', backgroundColor: '#202024' }}>
+      <StatusBar 
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+
       { fontsLoaded ? <Text>Hello World</Text> : <View /> }
     </View>
   );
